96d6c61731daa2b6b3c8fbeeba97ff6f17075ef9
choice <choice@choicedeMacBook-Pro.local>
2015-06-11 22:49:37 +0800

    zzz

--- WWGroupBarsView/WWGroupBarsView/AppDelegate.m
+++ WWGroupBarsView/WWGroupBarsView/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    //add xie ming ming 
     return YES;
 }
 
